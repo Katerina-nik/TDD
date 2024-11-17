07a7c006dfd7085d1bab2b6b295903753454d795
Katerina-nik <eka-pukhti@yandex.ru>
2024-11-17 12:55:51 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,8 +1,15 @@
 package ru.netology;
 
+import java.util.HashMap;
+
 public class PhoneBook {
-    public int add(String name, long number){
-        return 0;
+    static HashMap<String, Long> phoneBook = new HashMap<>();
+
+    public int add(String name, long number) {
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.put(name, number);
+        }
+        return phoneBook.size();
     }
 
 }
